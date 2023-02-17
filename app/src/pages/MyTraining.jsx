247635dd2f300dfb7e95941b2793ddaf5914c7fb
Rena Sakata <rena.sakata.cn@gmail.com>
2023-02-17 11:36:47 -0800

    Add card omponent with link

--- app/src/pages/MyTraining.jsx
+++ app/src/pages/MyTraining.jsx
@@ -1,11 +1,55 @@
+import { useState } from "react";
+import Grid from "@mui/material/Grid";
+import LessonCard from "../components/LessonCard";
+import lessons from "./dummy-lesson";
+
 const MyTraining = () => {
+  const ongoing = lessons.filter(
+    (lesson) => 0 < lesson.progress_status && lesson.progress_status < 100
+  );
+  const toTake = lessons.filter((lesson) => lesson.progress_status === 0);
+  const completed = lessons.filter((lesson) => lesson.progress_status === 100);
+
   return (
-      <>
-        <div>
-          <h2>This is my training</h2>
-        </div>
-      </>
-    );
+    <>
+      <h1>My Training</h1>
+      <h2>Ongoing Lessons</h2>
+      <Grid container spacing={1}>
+        {ongoing.map((lesson) => (
+          <LessonCard
+            key={lesson.id}
+            name={lesson.name}
+            progress={lesson.progress_status}
+            to={`/lesson/${lesson.id}`}
+          />
+        ))}
+      </Grid>
+
+      <h2>Lessons to Take</h2>
+      <Grid container spacing={1}>
+        {toTake.map((lesson) => (
+          <LessonCard
+            key={lesson.id}
+            name={lesson.name}
+            progress={lesson.progress_status}
+            to={`/lesson/${lesson.id}`}
+          />
+        ))}
+      </Grid>
+
+      <h2>Completed Lessons</h2>
+      <Grid container spacing={1}>
+        {completed.map((lesson) => (
+          <LessonCard
+            key={lesson.id}
+            name={lesson.name}
+            progress={lesson.progress_status}
+            to={`/lesson/${lesson.id}`}
+          />
+        ))}
+      </Grid>
+    </>
+  );
 };
 
 export default MyTraining;

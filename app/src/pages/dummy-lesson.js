d1cfe4422ffa5ad797f6083f24f6b4271a3aecaf
Rena Sakata <rena.sakata.cn@gmail.com>
2023-02-17 00:03:44 -0800

    Add card omponent with link

---
+++ app/src/pages/dummy-lesson.js
@@ -0,0 +1,48 @@
+// dummy data for lessons (cards)
+
+const lessons = [
+    {
+        id: 1,
+        name: "Latte",
+        progress_status: 30,
+        icon: "☕"
+    },
+    {
+        id: 2,
+        name: "Flat white",
+        progress_status: 0,
+        icon: "☕"
+    },
+    {
+        id: 3,
+        name: "Americano",
+        progress_status: 100,
+        icon: "☕"
+    },
+    {
+        id: 4,
+        name: "Espresso",
+        progress_status: 40,
+        icon: "☕"
+    },
+    {
+        id: 5,
+        name: "Mocha",
+        progress_status: 70,
+        icon: "☕"
+    },
+    {
+        id: 6,
+        name: "Caramel macciato",
+        progress_status: 100,
+        icon: "☕"
+    },
+    {
+        id: 7,
+        name: "London Fog",
+        progress_status: 0,
+        icon: "☕"
+    },
+];
+
+export default lessons;

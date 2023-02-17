4c57c2848a02f88649d6c8d7b849040be0bc4ccf
Rena Sakata <rena.sakata.cn@gmail.com>
2023-02-17 11:50:40 -0800

    Add card omponent with link

---
+++ app/src/components/LessonCard.jsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import Box from "@mui/material/Box";
+import LinearProgress from "@mui/material/LinearProgress";
+import Grid from "@mui/material/Grid";
+
+export default function LessonCard({ icon, name, progress, to }) {
+  return (
+    <Grid item xs={2}>
+      <Card sx={{ maxWidth: 200 }}>
+        <CardActionArea component={Link} to={to}>
+          <CardMedia component="img" height="140" image={icon} alt={name} />
+          <CardContent>
+            <Typography gutterBottom variant="p" component="div">
+              {name}
+            </Typography>
+            {0 < progress && progress < 100 ? (
+              <Box sx={{ width: "100%" }}>
+                <LinearProgress variant="determinate" value={progress} />
+              </Box>
+            ) : null}
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Grid>
+  );
+}
---
+++ app/src/pages/dummy-lesson.js
@@ -0,0 +1,66 @@
+// dummy data for lessons (cards)
+
+const lessons = [
+    {
+        id: 1,
+        name: "3 Cups Chemex",
+        progress_status: 30,
+        icon: "☕"
+    },
+    {
+        id: 2,
+        name: "Flat white",
+        progress_status: 0,
+        icon: "☕"
+    },
+    {
+        id: 3,
+        name: "Americano",
+        progress_status: 100,
+        icon: "☕"
+    },
+    {
+        id: 4,
+        name: "Espresso",
+        progress_status: 40,
+        icon: "☕"
+    },
+    {
+        id: 5,
+        name: "Mocha",
+        progress_status: 70,
+        icon: "☕"
+    },
+    {
+        id: 6,
+        name: "Caramel macciato",
+        progress_status: 100,
+        icon: "☕"
+    },
+    {
+        id: 7,
+        name: "London Fog",
+        progress_status: 0,
+        icon: "☕"
+    },
+    {
+        id: 8,
+        name: "Dalgona coffee",
+        progress_status: 10,
+        icon: "☕"
+    },
+    {
+        id: 9,
+        name: "Latte",
+        progress_status: 30,
+        icon: "☕"
+    },
+    {
+        id: 10,
+        name: "Hello Kitty",
+        progress_status: 100,
+        icon: "☕"
+    },
+];
+
+export default lessons;

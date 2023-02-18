bed4bdfa49bdbf4cc8b4574c03256f6e77572037
dkiatsongsak00 <dkiatsongsak00@mylangara.ca>
2023-02-17 23:29:03 -0800

    Create Questionnaire Result component

---
+++ app/src/components/Questionnaire/ResultPage.jsx
@@ -0,0 +1,12 @@
+const ResultPage = ({ score, totalQuestions }) => {
+  return (
+    <div className="result-page">
+      <h2>Quiz Result</h2>
+      <p>
+        You got {score} out of {totalQuestions} questions correct.
+      </p>
+    </div>
+  );
+};
+
+export default ResultPage;

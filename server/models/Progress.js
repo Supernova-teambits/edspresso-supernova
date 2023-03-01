61155a6204dd8ad07e91752d813deb7ed9413577
EC2 Default User <ec2-user@ip-172-31-25-178.us-west-1.compute.internal>
2023-03-01 00:02:41 +0000

    SUP-157 <update progress Schema>

--- server/models/Progress.js
+++ server/models/Progress.js
@@ -7,6 +7,7 @@ const ProgressSchema = new Schema({
     trainee_lesson: { type: String, required: true },
     manager_code: { type: String, required: true },
     progress_status: { type: Number, required: true },
+    completed_date: { type: Date },
     certification: { type: String },
     certification_date: { type: Date },
     test_result: { type: Number },

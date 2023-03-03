030f497e73016d6842ca3102db5808d8a4446e80
Rena Sakata <rena.sakata.cn@gmail.com>
2023-03-02 22:35:43 -0800

    Remove uused code

--- app/src/components/Card/LessonCard.jsx
+++ app/src/components/Card/LessonCard.jsx
@@ -3,28 +3,28 @@ import {
   Card,
   CardContent,
   CardMedia,
-  Typography,
   CardActionArea,
   Box,
   LinearProgress,
   Grid,
 } from "@mui/material";
 
-export default function LessonCard({ icon, title, progress, to, value, size=2 }) {
+export default function LessonCard({
+  icon,
+  title,
+  progress,
+  to,
+  value,
+  size = 2,
+}) {
   return (
     <Grid item xs={size}>
       <Card sx={{ maxWidth: 200 }}>
         <CardActionArea component={Link} to={to}>
           <CardMedia component="img" height="140" image={icon} alt={title} />
           <CardContent>
-            <Typography gutterBottom variant="p" component="div">
-              {title}
-            </Typography>
-            {value && (
-              <Typography gutterBottom variant="p" component="div">
-                {value}
-              </Typography>
-            )}
+            <p>{title}</p>
+            {value && <p>{value}</p>}
             {0 < progress && progress < 100 ? (
               <Box sx={{ width: "100%" }}>
                 <LinearProgress variant="determinate" value={progress} />

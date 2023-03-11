b965fe6e1e49810e6c6f75a5c8ce07787499aa1a
Nismath Naifar <naifarnismath@gmail.com>
2023-03-10 23:09:37 -0800

    conflict

--- app/src/assets/images.js
+++ app/src/assets/images.js
@@ -2,7 +2,7 @@ const loginBackground =
   "url(https://edspresso-bucket.s3.us-west-1.amazonaws.com/bg_login.jpg)";
 
 const chemex =
-  " https://images.pexels.com/photos/3749174/pexels-photo-3749174.jpeg";
+  "https://images.pexels.com/photos/3749174/pexels-photo-3749174.jpeg";
 
 const barista = () => {
   return (

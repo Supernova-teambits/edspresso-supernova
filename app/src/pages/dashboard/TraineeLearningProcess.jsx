d58f906193b5c61322025e0377c246f48cba2278
Jungho Lee <nightfallb612@gmail.com>
2023-03-14 23:56:30 -0700

    SUP-183 <CSS for Analytics - Learning process>

--- app/src/pages/dashboard/TraineeLearningProcess.jsx
+++ app/src/pages/dashboard/TraineeLearningProcess.jsx
@@ -1,7 +1,16 @@
-import { Card, CardContent } from "@mui/material";
+import {
+  Avatar,
+  Card,
+  CardContent,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
 import { useRecoilValue } from "recoil";
 import { filteredProgressByTraineeState } from "../../recoil/selectors";
 import { User } from "../../assets/Icons";
+import ProgressStatusLabel from "./ProgressStatusLabel";
 
 const TraineeLearningProcess = () => {
   const filteredProgressByTraineeList = useRecoilValue(
@@ -11,14 +20,23 @@ const TraineeLearningProcess = () => {
     <Card>
       <CardContent>
         <h3>Learning process</h3>
-        <ul>
-          {filteredProgressByTraineeList.map((item) => (
-            <li key={item.trainee_name + item.lesson_title}>
-              <User fillColor="#4E4F54" />
-              {item.trainee_name} | {item.status}
-            </li>
+        <List>
+          {filteredProgressByTraineeList.map((item, index) => (
+            <ListItem key={item.trainee_name + item.lesson_title}>
+              <ListItemAvatar>
+                <Avatar src={<User />} />
+              </ListItemAvatar>
+              <ListItemText
+                primary={item.trainee_name}
+                style={{ width: "160px" }}
+              />
+              <ListItemText>
+                <ProgressStatusLabel status={item.status} />
+                {/* <span>{item.status}</span> */}
+              </ListItemText>
+            </ListItem>
           ))}
-        </ul>
+        </List>
       </CardContent>
     </Card>
   );

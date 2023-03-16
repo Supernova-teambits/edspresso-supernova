7a6ea6703d9f3e369c7a675e66b69244f8ceb560
Rena Sakata <rena.sakata.cn@gmail.com>
2023-03-16 11:21:27 -0700

    apply css

--- app/src/components/Layout.jsx
+++ app/src/components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = () => {
         <Header />
         <SideBar />
       </div>
-      <div style={{ marginLeft: '260px' }}>
+      <div style={{ marginLeft: '200px' }}>
         <Outlet />
       </div>
     </div>

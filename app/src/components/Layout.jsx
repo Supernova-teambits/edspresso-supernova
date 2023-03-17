0f7aa2c99e22aef72e11d532e62785e4993b5786
Rena Sakata <rena.sakata.cn@gmail.com>
2023-03-17 09:44:46 -0700

    remove Header.jsx

--- app/src/components/Layout.jsx
+++ app/src/components/Layout.jsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
 import SideBar from "./SideBar";
 const Layout = () => {
   return (
     <div style={{ display: "flex" }}>
       <SideBar />
       <div class='content' style={{ width: '100%' }}>
-        <Header />
         <Outlet />
       </div>
     </div>

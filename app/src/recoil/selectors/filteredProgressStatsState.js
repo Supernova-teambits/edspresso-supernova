3f0531103508a3a582374ae5cdee67fe1526d9d4
Jungho Lee <nightfallb612@gmail.com>
2023-03-22 00:07:46 -0700

    BUG-33 <check zero value>

--- app/src/recoil/selectors/filteredProgressStatsState.js
+++ app/src/recoil/selectors/filteredProgressStatsState.js
@@ -10,15 +10,17 @@ export const filteredProgressStatsState = selector({
       (item) => item.progress_status === 100
     );
     const percentageCompleted =
-      totalNum === "0%"
-        ? 0
+      totalNum === 0
+        ? "0%"
         : Math.round((totalCompleted.length / totalNum) * 100) + "%";
 
     const sumOfCompletedTime = totalCompleted.reduce((result, item) => {
       return result + item.completed_time;
     }, 0);
     const avgTimeToCompleted =
-      Math.round(sumOfCompletedTime / totalCompleted.length) + " Mins";
+      sumOfCompletedTime === 0
+        ? "0 Min"
+        : Math.round(sumOfCompletedTime / totalCompleted.length) + " Mins";
     return {
       totalNum,
       totalCompleted,

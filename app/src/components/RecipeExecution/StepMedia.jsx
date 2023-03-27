ceabc9923ae671c7b9b319f1312d2d30133a7d95
Rena Sakata <rena.sakata.cn@gmail.com>
2023-03-26 21:36:45 -0700

    SUP-224 fix flex bug & accordion line

--- app/src/components/RecipeExecution/StepMedia.jsx
+++ app/src/components/RecipeExecution/StepMedia.jsx
@@ -43,14 +43,9 @@ export const IngredientsCard = ({ content }) => {
       {matches ? (
         <>
           <h4 className="StepMedia-title">{content.title}</h4>
-          <Grid container>
+          <Grid container spacing={1} xs={{width: "100%"}}>
             {content.content.map((el, index) => (
-              <LessonCard
-                key={index}
-                title={el.title}
-                value={el.value}
-                size={4}
-              />
+              <LessonCard key={index} title={el.title} value={el.value} />
             ))}
           </Grid>
         </>

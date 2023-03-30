e7ea069f2cc55ccde8957087a0a3991bdc82f2b3
Jungho Lee <nightfallb612@gmail.com>
2023-03-30 11:40:00 -0700

    BUG-46 <Set background color of the Dashboard>

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
-import "./App.scss";
 
 const router = createBrowserRouter(routes);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
